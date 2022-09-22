e478cd0d459d2a33b4f98a699799144210071b17
JHyun <wogustkfkd11@gmail.com>
2022-09-22 09:01:19 +0900

    class Num70 & fix solution commit

--- src/Leetcode/Solution.java
+++ src/Leetcode/Solution.java
@@ -1,4 +1,12 @@
 package Leetcode;
 
-public class Solution {
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
+        int cnt = climbStairs(n - 1) + climbStairs(n - 2);
+        return cnt;
+    }
 }
