0c31c207d2674a006c4996eecd0425bbe3c53261
JHyun <wogustkfkd11@gmail.com>
2022-09-22 16:18:15 +0900

    Num70 commit

--- src/Leetcode/Num70.java
+++ src/Leetcode/Num70.java
@@ -1,22 +1,17 @@
 package Leetcode;
 class Num70 {
-    int [] memo = new int[46];
-    public int climbStairs(int n) {
-        for(int i =0; i< 46; i++)
-            memo[i] = -1;
-
-        return climb(n);
-    }
-    public int climb(int n) {
-        if(memo[n] != -1)
-            return memo[n];
-        if (n == 0)
-            return 1;
-        if (n < 0)
-            return 0;
-        int cnt = climb(n - 1) + climb(n - 2);
-        memo[n] = cnt;
-        return cnt;
-    }
+        int [] memo = new int[46];
+        public int climbStairs(int n) {
+            if (n == 0)
+                return 1;
+            if (n < 0)
+                return 0;
+            if (memo[n] > 0)
+                return memo[n];
+            int cnt = climbStairs(n - 1) + climbStairs(n - 2);
+            memo[n] = cnt;
+            return cnt;
+        }
 }
+
 
