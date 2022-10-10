ff3c3054ca1ddf4f6d28c0cca8c5d4621aada0b8
JHyun <wogustkfkd11@gmail.com>
2022-10-10 11:08:22 +0900

    num207 commit

--- src/Leetcode/Num207.java
+++ src/Leetcode/Num207.java
@@ -3,40 +3,40 @@ package Leetcode;
 public class Num207 {
 
     static int i, j;
-    static int numCourses = 2;
-    static int[] visited = new int[numCourses];
-
-    static int[][] adj_mat = new int[numCourses][numCourses];
-
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
- /*       for(i = 0; i< numCourses; i++) {
+    public static boolean canFinish(int numCourses, int[][] prerequisites, int[] visited, int[][] adj_mat) {
+        int V = numCourses;
+        for (i = 0; i < numCourses; i++)
+            visited[i] = 0;
+        for (i = 0; i < numCourses; i++) {
             for (j = 0; j < numCourses; j++) {
-                if(prerequisites[i][j] == 1) {
-                    adj_mat[i][j] = prerequisites[i][j]
-                }
+                adj_mat[i][j] = 0;
             }
-        }*/
-
-        for(i = 0; i< prerequisites.length; i++) {
-            adj_mat[prerequisites[i][0]][prerequisites[i][1]] = 1;
         }
 
+        for(i = 0; i< prerequisites.length; i++) {
+            adj_mat[prerequisites[i][1]][prerequisites[i][0]] = 1;
+        }
         for(i =0; i<numCourses; i++){
+            if(adj_mat[i][i] == 1)
+                return false;
             for(j =0; j<numCourses; j++){
                 if(adj_mat[i][j] == 1 && adj_mat[j][i] == 1){
                     return false;
                 }
             }
         }
-        return true;
+        if(has_cycle(numCourses-1, visited, adj_mat, V) == 1)
+            return false;
+        else
+            return true;
     }
-/*    static int has_cycle( int n) {
+    public static int has_cycle(int n, int[] visited, int[][] adj_mat, int V) {
         int cycle = 0;
-        if(visited[i] == 0){
-            visited[i] = 1;
-            for(i=0; i<numCourses; i++){
+        if(visited[n] == 0){
+            visited[n] = 1;
+            for(i=0; i<V; i++){
                 if(adj_mat[n][i] == 1){
-                    if(has_cycle(i) == 1)
+                    if(has_cycle(i, visited, adj_mat, V) == 1)
                         return 1;
                 }
             }
@@ -45,9 +45,13 @@ public class Num207 {
         }
         else
             return 1;
-    }*/
+    }
 
     public static void main(String[] args) {
+        int numCourses = 2; //input
+        int[] visited = new int[numCourses+1];
+        int[][] adj_mat = new int[numCourses+1][numCourses+1];
+        int[][] prerequisites = new int[numCourses+1][numCourses+1];
         for (i = 0; i < numCourses; i++)
             visited[i] = 0;
         for (i = 0; i < numCourses; i++) {
@@ -55,6 +59,7 @@ public class Num207 {
                 adj_mat[i][j] = 0;
             }
         }
+        canFinish(numCourses, prerequisites, visited, adj_mat) { //prerequisites - input
 
     }
 }
