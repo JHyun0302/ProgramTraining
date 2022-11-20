a2248109b35e00e0fdcddb9001ab8386a24c6180
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-21 00:00:35 +0900

    leetcode random commit

--- src/Leetcode/Ramdomized/Num1515.java
+++ src/Leetcode/Ramdomized/Num1515.java
@@ -2,33 +2,69 @@ package Leetcode.Ramdomized;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Num1515 {
+    static int[][] positions = {{1, 1}, {0, 0}, {2, 0}};
+
+    static Random rand;
+    static List<Integer> xPoint = new ArrayList<>();
+    static List<Integer> yPoint = new ArrayList<>();
+
+
+    public static double euclideanDistance(double xCenter, double yCenter, double x1, double y1) {
+        return Math.sqrt(Math.pow((x1 - xCenter), 2) + Math.pow((y1 - yCenter), 2));
+    }
+
     public static double getMinDistSum(int[][] positions) {
-        List<Integer> xPoint = new ArrayList<>();
-        List<Integer> yPoint = new ArrayList<>();
         for (int i = 0; i < positions.length; i++) {
             xPoint.add(positions[i][0]);
             yPoint.add(positions[i][1]);
         }
-        double xCenter = 0;
-        double yCenter = 0;
-        for (Integer integer : xPoint) {
-            xCenter += integer;
-        }
-        for (Integer integer : yPoint) {
-            yCenter += integer;
-        }
-        xCenter /= xPoint.size();
-        yCenter /= yPoint.size();
 
-        double radius = Math.sqrt(Math.pow(xCenter - positions[0][0], 2) + Math.pow(yCenter - positions[0][1], 2));
-        double length = radius * xPoint.size();
-        return length;
+        rand = new Random();
+        int xMin = Integer.MAX_VALUE;
+        int xMax = Integer.MIN_VALUE;
+        int yMin = Integer.MAX_VALUE;
+        int yMax = Integer.MIN_VALUE;
+        for (Integer integer : xPoint) {
+            if (xMin > integer) {
+                xMin = integer;
+            }
+            if (xMax < integer) {
+                xMax = integer;
+            }
+        }
+
+        for (Integer integer : yPoint) {
+            if (yMin > integer) {
+                yMin = integer;
+            }
+            if (yMax < integer) {
+                yMax = integer;
+            }
+        }
+        double Xmiddle = Math.round((double) (xMin + xMax) / xPoint.size() * 100000) / 100000.0;
+        double Ymiddle = Math.round((double) (yMin + yMax) / yPoint.size() * 100000) / 100000.0;
+        System.out.println(Xmiddle);
+        System.out.println(Ymiddle);
+        while (true) {
+            double xCenter = xMin + rand.nextDouble() * (xMax - xMin);
+            double yCenter = yMin + rand.nextDouble() * (yMax - yMin);
+            System.out.println(xCenter);
+            System.out.println(yCenter);
+            double distanceSum = 0;
+            if (xCenter == Xmiddle && yCenter == Ymiddle) {
+                for (int i = 0; i < xPoint.size(); i++) {
+                    distanceSum += euclideanDistance(xCenter, yCenter, xPoint.get(i), yPoint.get(i));
+                }
+                return distanceSum;
+            }
+        }
     }
 
+
     public static void main(String[] args) {
-        int[][] positions = {{1, 1}, {0, 0}, {2, 0}};
         double result = getMinDistSum(positions);
         System.out.println(result);
     }
--- src/Leetcode/Ramdomized/Num398.java
+++ src/Leetcode/Ramdomized/Num398.java
@@ -3,14 +3,14 @@ package Leetcode.Ramdomized;
 import java.util.Random;
 
 public class Num398 {
-    int[] nums;
-    Random rand;
+    static int[] nums = {1, 2, 3, 3, 3};
+    static Random rand;
 
     public Num398(int[] nums) {
         this.nums = nums;
     }
 
-    public int pick(int target) {
+    public static int pick(int target) {
         rand = new Random();
         while (true) {
             int r = rand.nextInt(nums.length); // 0~ num길이 미만의 난수 생성
@@ -19,5 +19,10 @@ public class Num398 {
             }
 
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pick(3));
+
     }
 }
