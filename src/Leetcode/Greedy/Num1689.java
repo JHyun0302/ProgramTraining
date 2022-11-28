df1a30807a15b638bf9dfb17826acf8651d4c4c3
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-28 21:40:02 +0900

    leetcode 1689 commit

--- src/Leetcode/Greedy/Num1689.java
+++ src/Leetcode/Greedy/Num1689.java
@@ -1,20 +1,22 @@
 package Leetcode.Greedy;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Num1689 {
     public static int minPartitions(String n) {
-        List<Integer> number = new ArrayList<>();
-        int max = Integer.MIN_VALUE;
         /**
          * n의 각 자리 숫자를 받아서 number에 각자리를 저장
          * 각 자리 숫자를 비교해서 가장 큰 숫자가 정답
          */
+        int[] number = new int[n.length()];
+        int max = Integer.MIN_VALUE;
+
         for (int i = 0; i < n.length(); i++) {
-            number.add(Integer.valueOf(n.charAt(i)));
-            if (max < number.get(i)) {
-                max = number.get(i);
+            number[i] = n.charAt(i) - '0';
+        }
+
+        for (int i = 0; i < n.length(); i++) {
+            if (max < number[i]) {
+                max = number[i];
             }
         }
         return max;
