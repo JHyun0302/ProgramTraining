9d80d3f1759eabdf3997ecdd04cca21ba88b1ef4
JHyun <wogustkfkd11@gmail.com>
2022-11-28 16:45:16 +0900

    need to fix

--- src/Leetcode/Greedy/Num1689.java
+++ src/Leetcode/Greedy/Num1689.java
@@ -7,6 +7,10 @@ public class Num1689 {
     public static int minPartitions(String n) {
         List<Integer> number = new ArrayList<>();
         int max = Integer.MIN_VALUE;
+        /**
+         * n의 각 자리 숫자를 받아서 number에 각자리를 저장
+         * 각 자리 숫자를 비교해서 가장 큰 숫자가 정답
+         */
         for (int i = 0; i < n.length(); i++) {
             number.add(Integer.valueOf(n.charAt(i)));
             if (max < number.get(i)) {
