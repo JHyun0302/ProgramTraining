5937daaf61c647b313a8e2c50e5e17796cb147ce
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-30 14:33:48 +0900

    anotation add

--- src/Leetcode/Recursive/Num894.java
+++ src/Leetcode/Recursive/Num894.java
@@ -1,18 +1,25 @@
 package Leetcode.Recursive;
 
-class TreeNode {
-  int val;
-  TreeNode left;
-  TreeNode right;
-  TreeNode() {}
-  TreeNode(int val) { this.val = val; }
-  TreeNode(int val, TreeNode left, TreeNode right) {
-      this.val = val;
-     this.left = left;
-      this.right = right;
-  }
-}
+
 public class Num894 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 /*    List<TreeNode> root = new ArrayList<>();
     public List<TreeNode> allPossibleFBT(int n) {
         if(n == 1)
