9f3cb25027645ccdbcf67c4c1ae88c1e6cd191c3
JAEHYUN <wogustkfkd11@gmail.com>
2022-12-03 22:55:41 +0900

    밥

--- src/BaeJoon/Num23559.java
+++ src/BaeJoon/Num23559.java
@@ -1,21 +1,57 @@
 package BaeJoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Num23559 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int X = sc.nextInt();
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+    public static int selectLunch(int day, int money, int[][] menu, int sum) {
+        int i;
+        // 5000원짜리 맛 내림차순 정렬
+        Arrays.sort(menu, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                int oper1 = o1[1] - o1[0];
+                int oper2 = o2[1] - o2[0];
 
-        // -1 로 초기화
-        for(int i = 3; i < dp.length; i++) {
-            dp[i] = -1;
+                return oper1 - oper2;
+            }
+        });
+
+        int fiveThCnt = money / 5000;  // 5000원 개수
+        int oneThCnt = money % 5000;
+        for (i = 0; menu[i][0] > menu[i][1] && fiveThCnt > 0; i++) {
+            if (day >= 2 && money <= fiveThCnt * 5000) {
+                fiveThCnt = 0;
+            }
+            sum -= menu[i][1];
+            sum += menu[i][0];
+            fiveThCnt--;
+            day--;
         }
 
-        System.out.println(Tile(length));
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int day = Integer.parseInt(st.nextToken());
+        int money = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        int[][] menu = new int[day][2];
+
+        for (int i = 0; i < day; i++) {
+            st = new StringTokenizer(br.readLine());
+            menu[i][0] = Integer.parseInt(st.nextToken());
+            menu[i][1] = Integer.parseInt(st.nextToken());
+            sum += menu[i][1]; // 1000원권 모두 더하고 시작
+        }
+
+        System.out.println(selectLunch(day, money, menu, sum));
     }
 }
