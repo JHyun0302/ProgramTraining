4a1e7dcca037fa48ebd0860c82571b5ad4a3ea38
JAEHYUN <wogustkfkd11@gmail.com>
2022-12-04 20:16:20 +0900

    middle test

--- src/Middle_Test_Template/step_stone.java
+++ src/Middle_Test_Template/step_stone.java
@@ -6,25 +6,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class step_stone {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for(int i =0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int ans = 0;
             int size = Integer.parseInt(st.nextToken());
-            int []cost = new int [size];
+            int[] cost = new int[size];
             System.out.println("size: " + size);
-            for(int j = 0; j < size; j++) {
+            for (int j = 0; j < size; j++) {
                 int k = Integer.parseInt(st.nextToken());
                 cost[j] = k;
             }
-            for(int k = 2; k < cost.length; k++) {
-                cost[k] = Math.min(cost[k-1], cost[k-2]) + cost[k];
+            for (int k = 2; k < cost.length; k++) {
+                cost[k] = Math.min(cost[k - 1], cost[k - 2]) + cost[k];
 
             }
-            ans = Math.min(cost[cost.length- 1], cost[cost.length-2]);
+            ans = Math.min(cost[cost.length - 1], cost[cost.length - 2]);
             System.out.println(ans);
         }
     }
