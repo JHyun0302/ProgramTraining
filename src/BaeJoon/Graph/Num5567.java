67de369609be9fe79e53ef726dfdeb7bcb92ff22
JAEHYUN <wogustkfkd11@gmail.com>
2022-12-04 20:19:48 +0900

    결혼식

--- src/BaeJoon/Graph/Num5567.java
+++ src/BaeJoon/Graph/Num5567.java
@@ -7,20 +7,21 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Num5567 {
-    static boolean []visited;
+    static boolean[] visited;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
     public static void main(String[] args) throws NumberFormatException, IOException {
         // TODO Auto-generated method stub
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        visited = new boolean[n+1];
-        for(int  i =0; i < n+1; i++) {
+        visited = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
             graph.add(new ArrayList<>());
         }
 
         StringTokenizer st;
-        for(int i =0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -28,22 +29,22 @@ public class Num5567 {
             graph.get(b).add(a);
             System.out.println(graph);
         }
-        DFS(1,0);
+        DFS(1, 0);
         int cnt = 0;
-        for(int i = 2; i< visited.length;i++)
-            if(visited[i] == true)
+        for (int i = 2; i < visited.length; i++)
+            if (visited[i] == true)
                 cnt++;
         System.out.println(cnt);
     }
 
     static void DFS(int start, int depth) {
-        if(depth == 2)
+        if (depth == 2)
             return;
-        for(int i =0; i< graph.get(start).size();i++) {
+        for (int i = 0; i < graph.get(start).size(); i++) {
             int next = graph.get(start).get(i);
-            System.out.println(start+" "+next+ " " + depth);
+            System.out.println(start + " " + next + " " + depth);
             visited[next] = true;
-            DFS(next,depth+1);
+            DFS(next, depth + 1);
         }
     }
 }
