7e76bfeb7e6272155540f8fcbf6308fae5cfdfd8
JAEHYUN <wogustkfkd11@gmail.com>
2022-12-04 20:11:21 +0900

    middle test study

--- src/Middle_Test_Template/Can_Go_City.java
+++ src/Middle_Test_Template/Can_Go_City.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Can_Go_City {
@@ -20,34 +19,35 @@ public class Can_Go_City {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
-        visit = new int [N+1];
-        for(int  i =0; i < N+1; i++) {
+        visit = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) {
             graph.add(new ArrayList<>());
         }
-        for(int i =0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
             graph.get(v1).add(v2);
             graph.get(v2).add(v1);
         }
-        for(int i = 1; i <graph.size(); i++) {
+        for (int i = 1; i < graph.size(); i++) {
             Collections.sort(graph.get(i));
             // Collections.sort(graph.get(i), Comparator.reverseOrder()); 24480은 위 코드를 안쓰고 이 코드를 쓰면 됨
         }
 
         DFS(R);
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <visit.length;i++)
+        for (int i = 1; i < visit.length; i++)
             sb.append(visit[i] + "\n");
         System.out.println(sb);
 
     }
+
     static void DFS(int start) {
         visit[start] = cnt;
-        for(int i = 0; i<graph.get(start).size();i++) {
+        for (int i = 0; i < graph.get(start).size(); i++) {
             int next = graph.get(start).get(i);
-            if(visit[next] == 0) {
+            if (visit[next] == 0) {
                 cnt++;
                 DFS(next);
             }
