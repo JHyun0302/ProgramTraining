7d75f3d4404133974ac983d327cb44dfdfb7ed4e
JAEHYUN <wogustkfkd11@gmail.com>
2022-12-04 20:20:10 +0900

    미완성

--- src/BaeJoon/BackTracking/Num9663.java
+++ src/BaeJoon/BackTracking/Num9663.java
@@ -8,7 +8,8 @@ import java.util.StringTokenizer;
 public class Num9663 {
     static int N;
     static boolean[][] arr;
-    static int answer =0;
+    static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,20 +19,21 @@ public class Num9663 {
         dfs(0);
         System.out.println(answer);
     }
-    public static void dfs(int depth){
-        if(depth == N){
+
+    public static void dfs(int depth) {
+        if (depth == N) {
             answer++;
             return;
         }
 
-        for(int i = 0; i<N; i++){
-            for(int j =0; j<N; j++){
-                if(arr[i][j] == false){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == false) {
                     arr[0][j] = true;
                     arr[j][0] = true;
                     arr[j][j] = true;
                 }
-                dfs(depth+1);
+                dfs(depth + 1);
             }
 
         }
