a63e5f8e9b1b97fd8344441d036d8051522dc49a
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-17 10:21:22 +0900

    not sloving

---
+++ src/BaekJoon/Dynamic/Num1309.java
@@ -0,0 +1,18 @@
+package BaekJoon.Dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num1309 {
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 1][2];
+
+        
+    }
+}
